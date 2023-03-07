cea50d15c712b1f8192611a2e5f56c5b41a8695d
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-03-07 11:15:31 +0000

    [Add]管理者とユーザーの認証機能の実装、レイアウトの整理

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def self.guest
+    find_or_create_by!(email: 'guest@guest.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # end_user.confirmed_at = Time.now
+      user.name = "ゲスト"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope module: :public do
+    root to: 'homes#top'
+  end
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
+  end
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
+  # 顧客用
+  # URL /end_users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 end
