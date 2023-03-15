8d5607d2e9276dd4bca072838afdea4dff40984f
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-03-15 05:11:18 +0000

    [Add]タグの修正

--- app/controllers/admin/event_comments_controller.rb
+++ app/controllers/admin/event_comments_controller.rb
@@ -2,4 +2,9 @@ class Admin::EventCommentsController < ApplicationController
   def index
     @event_comments = EventComment.all
   end
+  
+  def destroy
+    EventComment.find(params[:id]).destroy
+    redirect_to admin_event_comments_path
+  end
 end
