cfd31fde1eb0e8a815f65f7a5de60468014dee88
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-03-21 07:40:07 +0000

    [update]イベント詳細ページレイアウト調整

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -40,6 +40,14 @@ class Public::SessionsController < Devise::SessionsController
       flash[:notice] = "項目を入力してください"
     end
   end
+
+  def after_sign_in_path_for(resource)
+    events_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
