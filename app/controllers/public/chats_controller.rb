3683f78638128085ab9ec149ba6db9488d37bd25
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-03-23 11:47:26 +0000

    [update]チャットの非同期通信化

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -3,10 +3,14 @@ class Public::ChatsController < ApplicationController
   def create
     @group = Group.find(params[:group_id]) #グループのidを探す
     @chat = Chat.new(chat_params)
-    if @chat.save
-      redirect_to event_group_path(@group.event, @group)
-    else
-      redirect_to event_group_path(@group.event, @group)
+    respond_to do |format|
+      if @chat.save
+        format.html { redirect_to @chat } # HTMLで返す場合、showアクションを実行し詳細ページを表示
+        format.js # create.js.erbが呼び出される
+      else
+        format.html { render :show } # HTMLで返す場合、show.html.erbを表示
+        format.js { render :errors } # 一番最後に実装の解説があります
+      end
     end
   end
 
