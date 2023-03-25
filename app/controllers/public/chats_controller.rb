ff889237cbac1ae6057e4547747b041bdc380ef6
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-03-25 07:20:16 +0000

    [update]チャット機能のバリデーション設定

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -3,14 +3,10 @@ class Public::ChatsController < ApplicationController
   def create
     @group = Group.find(params[:group_id]) #グループのidを探す
     @chat = Chat.new(chat_params)
-    respond_to do |format|
-      if @chat.save
-        format.html { redirect_to @chat } # HTMLで返す場合、showアクションを実行し詳細ページを表示
-        format.js # create.js.erbが呼び出される
-      else
-        format.html { render :show } # HTMLで返す場合、show.html.erbを表示
-        format.js { render :errors } # 一番最後に実装の解説があります
-      end
+    if @chat.save
+      
+    else
+      render '/public/groups/show'
     end
   end
 
