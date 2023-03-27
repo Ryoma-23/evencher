cf8daf98963aa43ed3ff11b8aec3e43605542475
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-03-27 07:17:23 +0000

    [add]seedファイルにユーザーを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@ Admin.create!(
   email: 'admin@admin',
   password: 'evencher'
 )
+
+users = User.create!(
+  [
+    {email: 'aaa@test.com', name: '一郎', password: '123456', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'bbb@test.com', name: '二郎', password: '123456', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'ccc@test.com', name: '三郎', password: '123456', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
