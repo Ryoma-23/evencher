a7f683ad00c2202f727023b10d7ba2d01916f113
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-04-18 06:52:08 +0000

    [update]管理者側ユーザー詳細レイアウト修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,7 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @event = @user.events.order(created_at: :desc)
+    @events = @user.events.page(params[:page]).order(created_at: :desc)
   end
 
   def edit
